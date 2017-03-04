cc15ac49a2698ce2ec0dfe9d9ae61392d42025eb
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2017-03-04 21:37:13 +0000

    Rigid Body skeleton and constructors

--- BladeEngine/include/rigid_body_component.h
+++ BladeEngine/include/rigid_body_component.h
@@ -10,17 +10,50 @@ namespace Blade
 
 	private:
 		//TO DO: Add all relevant members
+		Vec3d m_Velocity;
+		float m_Mass;
 	public:
-		RigidBodyComponent(Entity* parent);
+		/**
+		* \brief Constructs RigidBodyComponent with mass "mass"
+		* \param Parent enitity pointer - does not take ownership.
+		* \param Mass of the body - Kilograms assumed
+		*/
+		RigidBodyComponent(Entity* parent, float mass);
+
+
+		/**
+		* \brief Constructs RigidBodyComponent with mass "mass"
+		* \param Parent enitity pointer - does not take ownership.
+		* \param Density of the body - Kilograms/meters^3 assumed.
+		* \param Volume of the body -meters^3 assumed.
+		*/
+		RigidBodyComponent(Entity* parent, float density, float volume);
 
 		RigidBodyComponent(RigidBodyComponent&) = delete;
 		RigidBodyComponent& operator=(RigidBodyComponent&) = delete;
 		~RigidBodyComponent();
 
+		/**
+		* \brief Adds force to the rigid body.
+		* \param Force vector to add.
+		*/
+		void AddForce(Vec3d& f) noexcept;
+
+		/**
+		* \brief Adds force to the rigid body.
+		* \return Body velocity vector.
+		*/
+		Vec3d GetVelocity() const noexcept;
+
+		/**
+		* \brief Gets mass(kgs assumed).
+		* \return Mass in kilograms.
+		*/
+		float GetMass() const noexcept;
 		/**************** WILL BE REMOVED ONCE COMPONENT DEFINITION STUFF IS SORTED **********/
 
 		/**
-		* \brief Performs setup actions after the Component's creation
+		* \brief Performs setup actions after the Component's creation.
 		*/
 		void Setup() noexcept override;
 
--- BladeEngine/src/rigid_body_component.cpp
+++ BladeEngine/src/rigid_body_component.cpp
@@ -3,14 +3,38 @@
 
 namespace Blade
 {
-	RigidBodyComponent::RigidBodyComponent(Entity* parent) :
-		Component::Component{ "co_rb", parent }
+	RigidBodyComponent::RigidBodyComponent(Entity* parent, float mass) :
+		Component::Component{ "co_rb", parent },
+		m_Mass{mass}
 	{
 		EngineContext::GetSimulationSystem()->RegisterComponent(this);
+	}
+
+	RigidBodyComponent::RigidBodyComponent(Entity * parent, float density, float volume):
+		Component::Component{ "co_rb", parent },
+		m_Mass{ density* volume }
+	{
 	}
 
 
 	RigidBodyComponent::~RigidBodyComponent()
 	{
+		EngineContext::GetSimulationSystem()->UnregisterComponent(this);
 	}
+
+	void RigidBodyComponent::AddForce(Vec3d & f) noexcept
+	{
+		m_Velocity += f;
+	}
+
+	Vec3d RigidBodyComponent::GetVelocity() const noexcept
+	{
+		return m_Velocity;
+	}
+
+	float RigidBodyComponent::GetMass() const noexcept
+	{
+		return m_Mass;
+	}
+
 }
