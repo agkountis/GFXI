c5242f82e469124c81f2424f2d6b07947b6832d3
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2017-03-04 18:24:21 +0000

    Added conditional tracing macros for windows debug.

--- BattleArena/src/main.cpp
+++ BattleArena/src/main.cpp
@@ -1,14 +1,19 @@
 #include "battle_arena_application.h"
+#include "blade_trace.h"
+
 
 int main(int argc, char** argv)
 {
 	BattleArenaApplication application;
+	
 
 	if (!application.Initialize(&argc, argv))
 	{
 		return 1;
 	}
-
+	BLADE_TRACE_VSOUT("APP INITIALIZED.."); // viusal studio output printout
+	BLADE_TRACE_CON("APP INITIALIZED.."); // console output printout
+	BLADE_TRACE("CONSOLE AND VS PRINTOUT.."); // console and vs output printout
 	application.Run();
 
 	return 0;
