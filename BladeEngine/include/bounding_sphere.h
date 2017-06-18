76d7182d59bac91ac90ce0fe9d4e907a43cafa09
Kuba <K.maruszczyk@2013.hull.ac.uk>
2017-06-18 16:14:10 +0100

    Colliders docs cave

--- BladeEngine/include/bounding_sphere.h
+++ BladeEngine/include/bounding_sphere.h
@@ -19,12 +19,38 @@ namespace Blade
 	public:
 		explicit BoundingSphere(float radius);
 
+
+		/*
+		* \brief General method for collision detection with other collider.
+		* \param collider Pointer to other collider.
+		* \param manifold Reference to contact manifold to which the information about the collision will be added.
+		* \remarks Other collider represents either BoundingSphere or PlaneCollider.
+		* \return True if collision occured and entry was added to contact manifold, false otherwise
+		*/
 		bool Collide(const Collider* collider, ContactManifold& manifold) const noexcept override;
 
+
+		/*
+		* \brief Method for collision detection between this collider and Bounding Sphere collider.
+		* \param bsphere Pointer to other collider - bounding sphere.
+		* \param manifold Reference to contact manifold to which the information about the collision will be added.
+		* \return True if collision occured and entry was added to contact manifold, false otherwise
+		*/
 		bool Collide(const BoundingSphere* bsphere, ContactManifold& manifold) const noexcept override;
 
+		/*
+		* \brief Method for collision detection between this collider and plane collider.
+		* \param plane Pointer to other collider - plane colider.
+		* \param manifold Reference to contact manifold to which the information about the collision will be added.
+		* \return True if collision occured and entry was added to contact manifold, false otherwise
+		*/
 		bool Collide(const PlaneCollider* plane, ContactManifold& manifold) const noexcept override;
 
+
+		/*
+		* \brief Returns readius of the bounding sphere collider.
+		* \return readius of the bounding sphere collider
+		*/
 		const float GetRadius() const noexcept;
 	};
 }
